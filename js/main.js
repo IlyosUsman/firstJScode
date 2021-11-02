ddbe7e4be29286094d46a37ea3345bb99f3cea5b
IlyosUsman <91330828+IlyosUsman@users.noreply.github.com>
2021-11-03 00:55:28 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// start
+
+var yourName = prompt("What's your name?");
+
+var yourUsername = prompt("What's your username");
+
+var yourAge = prompt("How old are you?");
+
+var yourCity = prompt("Where do you live?");
+
+var yourDreamProfession = prompt("What's you dream profession?");
+
+var yourFavoritePlace = prompt("What's you favorite place in your city?");
+
+var what_did_you_do = prompt("What did you do yesterday?");
+
+var what_sport_do_you_know = prompt("What sports or activeties do you know?");
+
+var yourFavoriteSeason = prompt("What's your favorite season?");
+
+var yourFavoriteFood = prompt("What's your favorite food?");
+
+var do_you_like_shopping = prompt("Do you like shopping?");
+
+var why = prompt("why or why not?");
+
+var fullinfo = 
+"Name:" + " " + yourName + "\n" +
+"Username" + " " + yourUsername + "\n" +
+"Age" + " " + yourAge + "\n" + 
+"City" + " " + yourCity + "\n" +
+"DreamProfession" + " " + yourDreamProfession + "\n" +
+"FavouritePlace" + " " + yourFavoritePlace + "\n" +
+"_did_you_do" + " " + what_did_you_do + "\n" +
+"_sport_do_you_know" + " " + what_sport_do_you_know +"\n" +
+"FavoriteSeason" + " " + yourFavoriteSeason + "\n" +
+"FavoriteFood" + " " + yourFavoriteFood + "\n" +
+"_you_like_shopping" + " " + do_you_like_shopping + "\n" +
+"why" + " " + why + "\n" +
+"Thank you for attenciton" ;
+
+console.log(fullinfo);
+
+alert(fullinfo);
+
+// finish
